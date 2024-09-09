5093825e75ec147663d4eb1764664300cf167105
Danila Makarov <57109784+dqnilka@users.noreply.github.com>
2024-09-09 21:01:35 +0300

    CORD added-tg

--- newsletter-service/tg-bot/main.py
+++ newsletter-service/tg-bot/main.py
@@ -1,16 +1,19 @@
 import telebot
 from flask import Flask, request, jsonify
 import threading
+from flask_cors import CORS  
 
 TELEGRAM_TOKEN = '7304368665:AAHaDslyPe06nmsvihiK9AKbrRWIv6FAEDA'
 USER_ID = '465391024'
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 app = Flask(__name__)
+CORS(app)  
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Привет! Я бот, который дублирует твои сообщения.")
+    bot.send_message(USER_ID, text="test")
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
@@ -18,7 +21,16 @@ def echo_message(message):
 
 @app.route('/send-message', methods=['POST'])
 def send_message():
-    bot.send_message(USER_ID, text="test")
+    data = request.json
+    message = data.get('message', 'Тестовое сообщение!')
+    
+    try:
+        bot.send_message(USER_ID, text=message)
+        print("Сообщение отправлено:", message) 
+        return jsonify({'success': True, 'message': 'Сообщение отправлено!'}), 200
+    except Exception as e:
+        print("Ошибка отправки:", str(e)) 
+        return jsonify({'success': False, 'error': str(e)}), 500
 
 def start_bot():
     bot.polling()
