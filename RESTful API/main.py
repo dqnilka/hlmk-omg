be97f4c84418f92c6f744d677bddee22e7c9d607
Maxim Yamkin <makson.yamkin@mail.ru>
2024-09-13 12:16:21 +0300

    some troubles with model

--- RESTful API/main.py
+++ RESTful API/main.py
@@ -1,11 +1,18 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import sys
-sys.path.append('../ML')
-from get_query import get_answer
+import os
+import openai
+from jinja2 import Template
+
+from utils.prompt_template import PROMPT_TEMPLATE
+from utils.components_base import COMPONENTS_BASE
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+# Укажи свой ключ API здесь
+openai.api_key = os.environ.get('OPEN_AI_TOKEN')
 
 
 @app.route("/api/main", methods=["POST"])
@@ -49,6 +56,43 @@ def get_component_data():
     ]
 
     return jsonify({'components': components})
+
+
+def get_gpt_response(task):
+    try:
+        # Отправляем запрос к модели
+        response = openai.completions.create(
+            model="gpt-4",  # Используем модель GPT-4 или другую
+            prompt=task,
+            max_tokens=5000,  # Максимальное количество токенов в ответе
+            temperature=0.7  # Степень креативности ответов
+        )
+
+        # Получаем текст ответа
+        return response.choices[0].text.strip()
+
+    except Exception as e:
+        return f"Произошла ошибка: {e}"
+
+
+# здесь какой-то инпут
+def get_task(answer_front):
+    text = Template('''
+    {{answer_front}}
+    ''')
+    task = text.render(answer_front=answer_front)
+    print(task)
+    return task
+
+
+def get_answer(answer_front):
+    # Укажи свой ключ API здесь
+    task = get_task(answer_front)
+    template = Template(PROMPT_TEMPLATE)
+    query = template.render(task=task, components_base=COMPONENTS_BASE)
+    response = get_gpt_response(query)
+
+    return response
 
 
 if __name__ == "__main__":
